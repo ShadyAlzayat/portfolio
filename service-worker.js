07a32f866e9f7b47e71a4a38a68ed58c09ffde13
Shady Alzayat <emailshady@gmail.com>
2019-09-09 17:36:31 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shadyalzayat.github.io/precache-manifest.4e6890fda32134a354a3a0a7caec389d.js"
+  "/shadyalzayat.github.io/precache-manifest.0d331849bffa50b3a30a150c7282a8c3.js"
 );
 
 self.addEventListener('message', (event) => {
